a06f94b1ee300b0670bdbe32f0079b5720612b98
protocol42 <88254215+protocol42@users.noreply.github.com>
2023-05-30 03:48:49 -0600

    Update myapp.js
    added webpages

--- myapp.js
+++ myapp.js
@@ -1,11 +1,50 @@
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5500
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// homepage
+app.get('/', (request, response) => response.send(`
+  <h1>'Hello World!'<h1>
+  <a href='/contact'>Contact</a>
+  <a href='/about'>About</a>
+  <a href='/ourteam'>Ourteam</a>
+  <a href='/products'>Products</a>
+  `))
 
+
+// contact page
+app.get('/contact', (request, response) => response.send(`
+  <h1>Contact</h1>
+  <p>contact page</p>
+  `))
+
+// about page
+app.get('/about', (request, response) => response.send(`
+  <h1>About</h1>
+  <p>about page</p>
+  <a href='/ourteam'>Ourteam</a>
+`));
+
+// ourteam page
+app.get('/ourteam', (request, response) => response.send(`
+<h1>Ourteam</h1>
+<p>our team page</p>
+`))
+
+
+// products page
+app.get('/products', (request, response) => response.send(`
+<h1>Products</h1>
+<p>products page</p>
+`))
+
+
+
+
+// localhost:5500 - in browser
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
+
+
